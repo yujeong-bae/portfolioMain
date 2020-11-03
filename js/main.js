96352c1c44037edfc2633312b09d558a7a3b50f2
yujeong-bae <71867598+yujeong-bae@users.noreply.github.com>
2020-11-03 11:56:13 +0900

    1103ver3

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-
+    $('.pf-dmt-sub').stop().slideUp(10);
 
     $(window).scroll(function () {
         var scrollTop = $(window).scrollTop();
         var screen = $(window).scrollTop() + $(window).height();
-       
+
         $('section').each(function () {
-           
+
             var $sec = $(this);
             //active addclass 사용시
             if (($sec.offset().top + $sec.height() - 550) < screen) {
@@ -34,14 +34,34 @@ $(document).ready(function () {
         });
     });
 
+    //pc용 햄버거
+    function isMobile() {
+        var filter = "win32|win64|mac|macintel";
+        if (navigator.platform) {
+            if (filter.indexOf(navigator.platform.toLowerCase()) < 0) {
+                //mob
+                //    모바일 nav클릭시 꺼지기
+                $('.pf-dmt-sub li a').click(function(){
+                    $('header nav').stop().removeClass('active');
+                    
+                });
 
-   
+            } else {
+                $('.pf-dmt-main').hover(function () {
+                    $('.pf-dmt-sub').stop().slideDown(200);
+                });
+                $('.pf-dmt-main').mouseleave(function () {
+                    $('.pf-dmt-sub').stop().slideUp(800);
+                });
+            }
+        }
+    }
+    isMobile();
+
+
+    //햄버거
     $(".hamburger").click(function () {
         $(this).toggleClass("is-active");
         $('header nav').toggleClass('active');
     });
-
-
-   
-
 });
