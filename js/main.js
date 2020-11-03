6b24b6935d683a79979b8b1b622479bb5a2487e9
yujeong-bae <71867598+yujeong-bae@users.noreply.github.com>
2020-11-03 14:42:47 +0900

    v5

--- js/main.js
+++ js/main.js
@@ -1,38 +1,7 @@
 $(document).ready(function () {
     $('.pf-dmt-sub').stop().slideUp(10);
 
-    $(window).scroll(function () {
-        var scrollTop = $(window).scrollTop();
-        var screen = $(window).scrollTop() + $(window).height();
 
-        $('section').each(function () {
-
-            var $sec = $(this);
-            //active addclass 사용시
-            if (($sec.offset().top + $sec.height() - 550) < screen) {
-                $sec.find('.container').each(function () {
-                    var num = $(this).index();
-                    $(this).delay(num * 1000).addClass('active', 1000);
-                });
-            } else {
-                $sec.find('.container').each(function () {
-                    var num = $(this).index();
-                    $(this).delay(num * 1000).removeClass('active', 100);
-                });
-            }
-        });
-        if (scrollTop > 600) {
-            //header에 sticky추가
-            $('.up').addClass('active');
-        } else {
-            $('.up').removeClass('active');
-        }
-        $('.up').click(function () {
-            $('html,body').stop().animate({
-                scrollTop: 0
-            }, 800);
-        });
-    });
 
     //pc용 햄버거
     function isMobile() {
@@ -52,6 +21,39 @@ $(document).ready(function () {
                 });
                 $('.pf-dmt-main').mouseleave(function () {
                     $('.pf-dmt-sub').stop().slideUp(800);
+                });
+
+                $(window).scroll(function () {
+                    var scrollTop = $(window).scrollTop();
+                    var screen = $(window).scrollTop() + $(window).height();
+
+                    $('section').each(function () {
+
+                        var $sec = $(this);
+                        //active addclass 사용시
+                        if (($sec.offset().top + $sec.height() - 550) < screen) {
+                            $sec.find('.container').each(function () {
+                                var num = $(this).index();
+                                $(this).delay(num * 1000).addClass('active', 1000);
+                            });
+                        } else {
+                            $sec.find('.container').each(function () {
+                                var num = $(this).index();
+                                $(this).delay(num * 1000).removeClass('active', 100);
+                            });
+                        }
+                    });
+                    if (scrollTop > 600) {
+                        //header에 sticky추가
+                        $('.up').addClass('active');
+                    } else {
+                        $('.up').removeClass('active');
+                    }
+                    $('.up').click(function () {
+                        $('html,body').stop().animate({
+                            scrollTop: 0
+                        }, 800);
+                    });
                 });
             }
         }
