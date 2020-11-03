c2d74f475872e5261f1ebaf96ecd01fa23052e9d
yujeong-bae <71867598+yujeong-bae@users.noreply.github.com>
2020-11-03 14:48:01 +0900

    v4x

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
 $(document).ready(function () {
     $('.pf-dmt-sub').stop().slideUp(10);
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        var screen = $(window).scrollTop() + $(window).height();
 
+        $('section').each(function () {
+
+            var $sec = $(this);
+            //active addclass 사용시
+            if (($sec.offset().top + $sec.height() - 550) < screen) {
+                $sec.find('.container').each(function () {
+                    var num = $(this).index();
+                    $(this).delay(num * 1000).addClass('active', 1000);
+                });
+            } else {
+                $sec.find('.container').each(function () {
+                    var num = $(this).index();
+                    $(this).delay(num * 1000).removeClass('active', 100);
+                });
+            }
+        });
+        if (scrollTop > 600) {
+            //header에 sticky추가
+            $('.up').addClass('active');
+        } else {
+            $('.up').removeClass('active');
+        }
+        $('.up').click(function () {
+            $('html,body').stop().animate({
+                scrollTop: 0
+            }, 800);
+        });
+    });
 
 
     //pc용 햄버거
@@ -23,38 +54,7 @@ $(document).ready(function () {
                     $('.pf-dmt-sub').stop().slideUp(800);
                 });
 
-                $(window).scroll(function () {
-                    var scrollTop = $(window).scrollTop();
-                    var screen = $(window).scrollTop() + $(window).height();
 
-                    $('section').each(function () {
-
-                        var $sec = $(this);
-                        //active addclass 사용시
-                        if (($sec.offset().top + $sec.height() - 550) < screen) {
-                            $sec.find('.container').each(function () {
-                                var num = $(this).index();
-                                $(this).delay(num * 1000).addClass('active', 1000);
-                            });
-                        } else {
-                            $sec.find('.container').each(function () {
-                                var num = $(this).index();
-                                $(this).delay(num * 1000).removeClass('active', 100);
-                            });
-                        }
-                    });
-                    if (scrollTop > 600) {
-                        //header에 sticky추가
-                        $('.up').addClass('active');
-                    } else {
-                        $('.up').removeClass('active');
-                    }
-                    $('.up').click(function () {
-                        $('html,body').stop().animate({
-                            scrollTop: 0
-                        }, 800);
-                    });
-                });
             }
         }
     }
